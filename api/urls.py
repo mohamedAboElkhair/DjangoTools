e062ae437f93ad4b5c046b4bfe32837f672ad6ca
Mohamed Abo Elkhair <mohamedAboElkhair@users.noreply.github.com>
2023-03-30 11:48:05 +0200

    Delete api directory

--- api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from rest_framework.authtoken.views import obtain_auth_token
-from . import views
-urlpatterns = [
-   path('auth/',obtain_auth_token),
-   path('',views.api_tes) ,
-   # path("produ/", include('produ.urls')),
-]
