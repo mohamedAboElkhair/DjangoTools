0a16fdcdd74e19a5784e890283862fa1755526a2
Mohamed Abo Elkhair <mohamedAboElkhair@users.noreply.github.com>
2023-03-30 11:48:33 +0200

    Delete py_client_test directory

--- py_client_test/detail.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-urlpoint  = "http://127.0.0.1:8000/api/produ/1/"
-get_response = requests.get(urlpoint)
-#print(get_response.headers)
-print(get_response.json())
-# print(get_response.status_code)
